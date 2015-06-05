bf7d5ec5bf84cda73fa646eca7be2f4c0791c526
Manabu ISHII <manabu.ishii.rb@gmail.com>
2015-06-05 11:49:11 +0900

    bug fix: typo

--- recipes/sailfish_index_download.rb
+++ recipes/sailfish_index_download.rb
@@ -8,20 +8,26 @@
 
 # download generated index or execute sailfish command
 # cd /usr/local/galaxy/bit_tools/ ; wget "http://q-brain.riken.jp/vm/sailfish_index_20150529.tar.gz" -O - | tar zxvf -
-sailfix_index_command="touch /tmp/sailfish.txt"
+sailfish_index_command="touch /tmp/sailfish.txt"
 sailfish_index_create_method = node[:basicsetup][:sailfish][:index][:create_method]
 if sailfish_index_create_method == "executecommand"
   #sailfix_index_command = "source /etc/profile ; source /tmp/galaxy_tools/galaxy-mytools_setup-master/.venv/bin/activate ;  cd /tmp/galaxy_tools/galaxy-mytools_setup-master/setup_reference_and_index ; python setup_sailfish_in_Galaxy.py index_file_list.txt galaxy"
   sailfish_index_command = "touch /tmp/sailfishexecutecommand.txt"
 elsif sailfish_index_create_method == "download"
-  sailfish_index_command = "touch /tmp/sailfishdownload.txt"
+  
+  #sailfish_index_command = "touch /tmp/sailfishdownload.txt"
+  sailfish_index_command = "wget http://q-brain.riken.jp/vm/sailfish_index_20150529.tar.gz -O - | tar zxvf - -C /usr/local/galaxy/bit_tools/"
+else
+  sailfish_index_command = "touch /tmp/nevermatch.txt"
 end
 script "sailffish_index_create" do
   environment  ({ "HOME" => "/usr/local/galaxy" })
   interpreter "bash"
   user        "galaxy"
   code <<-EOL
-    #{sailfix_index_command}
+    #{sailfish_index_command}
+    echo "#{sailfish_index_create_method}" > /tmp/method
   EOL
   action :run
+  not_if { File.exists?("/usr/local/galaxy/bit_tools/sailfish_index/")}
 end
