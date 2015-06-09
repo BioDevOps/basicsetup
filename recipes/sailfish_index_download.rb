23565e250678f0b8097e59896fe5c09c73e80555
Manabu ISHII <manabu.ishii.rb@gmail.com>
2015-06-09 14:32:43 +0900

    change variable set

--- recipes/sailfish_index_download.rb
+++ recipes/sailfish_index_download.rb
@@ -9,14 +9,14 @@
 # download generated index or execute sailfish command
 sailfish_index_command="touch /tmp/sailfish.txt"
 sailfish_index_create_method = node[:basicsetup][:sailfish][:index][:create_method]
+sailfish_index_filename=node[:basicsetup][:sailfish][:index][:download][:filename]
+sailfish_index_url=node[:basicsetup][:sailfish][:index][:download][:url]
+
 if sailfish_index_create_method == "executecommand"
   #sailfix_index_command = "source /etc/profile ; source /tmp/galaxy_tools/galaxy-mytools_setup-master/.venv/bin/activate ;  cd /tmp/galaxy_tools/galaxy-mytools_setup-master/setup_reference_and_index ; python setup_sailfish_in_Galaxy.py index_file_list.txt galaxy"
   sailfish_index_command = "touch /tmp/sailfishexecutecommand.txt"
 elsif sailfish_index_create_method == "download"
   # download file
-  sailfish_index_filename=node[:basicsetup][:sailfish][:index][:download][:filename]
-  sailfish_index_url=node[:basicsetup][:sailfish][:index][:download][:url]
-
   remote_file "#{Chef::Config['file_cache_path']}/"+sailfish_index_filename do
     source sailfish_index_url
     mode   0644
