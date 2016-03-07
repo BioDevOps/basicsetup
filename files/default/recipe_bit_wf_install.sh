93ea57ae4cd7b9535b2fdcc788c7bec46fa5fd75
Manabu ISHII <manabu.ishii.rb@gmail.com>
2016-03-07 15:42:24 +0900

    Install pip in galaxy virtualenv environment

--- files/default/recipe_bit_wf_install.sh
+++ files/default/recipe_bit_wf_install.sh
@@ -1,3 +1,4 @@
 cd /usr/local/galaxy/work/galaxy_in_docker_custom_bit_wf/setup_scripts
+source /usr/local/galaxy/galaxy-dist/.venv/bin/activate; pip install python-dateutil bioblend pandas grequests GitPython pip-tools
 python bit-workflow_install_docker.py
 
