a6a651a0bcf8016b26663e5f7dcf953c68ae7f5c
Manabu ISHII <manabu.ishii.rb@gmail.com>
2016-03-15 14:29:11 +0900

    release 1.2.0

--- files/default/recipe_bit_wf_install.sh
+++ files/default/recipe_bit_wf_install.sh
@@ -1,4 +1,4 @@
 cd /usr/local/galaxy/work/galaxy_in_docker_custom_bit_wf/setup_scripts
-source /usr/local/galaxy/galaxy-dist/.venv/bin/activate; pip install python-dateutil bioblend pandas grequests GitPython pip-tools
+source /usr/local/galaxy/galaxy-dist/.venv/bin/activate; pip install python-dateutil==2.5.0 bioblend==0.7.0 pandas==0.17.1 grequests==0.3.0 GitPython==1.0.2 pip-tools==1.6
 python bit-workflow_install_docker.py
 
