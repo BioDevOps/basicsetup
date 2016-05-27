6f3e2d79f15a2c1dce58c7bcb8f2d5dd5dce3440
Manabu ISHII <manabu.ishii.rb@gmail.com>
2016-05-27 15:06:19 +0900

    Improve timeout to 10800 when med-bio install

--- recipes/med-bio-install.rb
+++ recipes/med-bio-install.rb
@@ -5,11 +5,14 @@
 # Copyright (c) [2015] [Manabu ISHII] and RIKEN Bioinformatics Research Unit
 # Released under the [Apache2 license]
 #
+
+# Get 2GB packages and extract them use 6GB space
 case node['platform']
 when "ubuntu"
   execute "ufw" do
     command <<-EOF
 DEBIAN_FRONTEND=noninteractive apt-get -y install med-bio
   EOF
+    timeout 10800
   end
 end
